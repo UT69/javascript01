e437f95929e373ee94bedf7121592fe1c60fe784
UT69 <146042423+UT69@users.noreply.github.com>
2024-01-02 13:19:02 +0000

    hi

--- 01.basics/co.js
+++ 01.basics/co.js
@@ -4,10 +4,13 @@ console.log(typeof(score));
 
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
+
 // NaN = NOT A NUMBER
 // "33" => 33
 // "33abc" => NaN
 // true => 1, false => 0
 // "" => false
 // "utk" => true
+
+
 
---
+++ 01.basics/datatypes.summary.js
@@ -0,0 +1,44 @@
+// the way data are kept and accessed in memory, 2 types of datatypes :-
+// 1> Primitive datatypes :-
+// 7 types : string, Number, Boolean, null,undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100;
+const isLoggedIn= false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const bigNumber = 23435266574788n //BigInt datatype
+
+
+// 2>Refernce (non-primitive):-
+//Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"] //arrays
+
+//objects
+let myObj= {
+    name :"utkarsh",
+    age: 22,
+
+}
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof(bigNumber));
+/*Type	Result
+Undefined	"undefined"
+Null	"object" (reason)
+Boolean	"boolean"
+Number	"number"
+BigInt	"bigint"
+String	"string"
+Symbol	"symbol"
+Function (implements [[Call]] in ECMA-262 terms; classes are functions as well)	"function"
+Any other object	"object"*/
+
