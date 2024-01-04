a7c326c17308dfb3dcd0a1308f45eb6e3ee7071f
UT69 <146042423+UT69@users.noreply.github.com>
2024-01-04 15:28:26 +0000

    yo

--- 01.basics/strings.js
+++ 01.basics/strings.js
@@ -1,3 +1,16 @@
 const name = "utkarsh"
 const repoCount = 50
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('utkarshhc')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf(2));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+
